e9948d1aeaace8400468bb3fcc959e2eb1ea64bf
abontech <2013tabomtech@gmail.com>
2024-10-07 03:28:50 -0700

    new chnages

--- src/pages/Job_provider.js
+++ src/pages/Job_provider.js
@@ -328,7 +328,7 @@ const handleRemoveImage = (index) => {
        
         <button
           type="button"
-          onClick={(e)=>{handleSubmit(e,"active")}}
+          onClick={(e)=>{handleSubmit(e,"draft")}}
           className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded flex items-center"
         >
          Draft

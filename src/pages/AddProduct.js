ca2e08424411e9ef38bebc9c8b0296a2fa464236
samiullah239 <2014tabontech@gmail.com>
2026-02-09 15:11:46 +0500

    done

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -9172,7 +9172,7 @@ sessionStorage.setItem(
                 <div className="w-14 h-14 border-4 border-white border-t-transparent rounded-full animate-spin" />
 
                 <p className="mt-5 text-white text-sm font-medium tracking-wide">
-                  Uploading product, please wait...
+                  please wait  while we {isEditing ? "update": "create"} your product...
                 </p>
               </div>
             )}
